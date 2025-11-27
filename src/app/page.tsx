342651003a97ae1d997d81293564c66a8c299937
enrico-zd <enricozada00@gmail.com>
2025-11-27 16:21:08 +0700

    chore: initial project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import CardReview from "@/components/CardReview/CardReview";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <CardReview />
+    </div>
+  );
+}
---
+++ src/components/CardReview/CardReview.tsx
@@ -0,0 +1,15 @@
+const CardReview = () => {
+    return (
+        <div>
+            {/* menambah avatar user */}
+            {/* beri icon 5 bintang */}
+            {/* data body size contoh: 165cm 65kg 88/78/110 cm */}
+            {/* tombol icon like dan lable review review */}
+            {/* text review dari user */}
+            {/* field gambar dari product review */}
+            {/* bulan, tanggal dan tahun dari review */}
+        </div>
+    )
+}
+
+export default CardReview;
